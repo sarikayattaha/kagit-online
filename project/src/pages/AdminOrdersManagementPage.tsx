6998b687559cd9f215e316e49475973c0696564d
sarikayattaha <sarikayattaha@gmail.com>
2025-10-16 18:57:15 +0300

    Update AdminOrdersManagementPage.tsx

--- project/src/pages/AdminOrdersManagementPage.tsx
+++ project/src/pages/AdminOrdersManagementPage.tsx
@@ -217,6 +217,8 @@ export default function AdminOrdersManagementPage() {
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
                 <input
+                  ıd="order-search-input"
+                  name="search"
                   type="text"
                   placeholder="Sipariş no, müşteri veya ürün ara..."
                   value={searchTerm}
