ebcb9431a0bdd35e34daa99b03dda4860c19ad54
sarikayattaha <sarikayattaha@gmail.com>
2025-10-17 11:52:12 +0300

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  build: {
+    chunkSizeWarningLimit: 1000, // 500'den 1000'e çıkar
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          supabase: ['@supabase/supabase-js'],
+        }
+      }
+    }
+  }
+})
